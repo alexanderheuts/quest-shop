7c4db80e66d082ce5e6b2508287e6d1436731ef4
alexanderheuts <alexanderheuts@users.noreply.github.com>
2025-09-14 13:26:12 +0200

    Added coin hovertext & foil.

--- src/main/java/com/holysweet/questshop/item/CoinItem.java
+++ src/main/java/com/holysweet/questshop/item/CoinItem.java
@@ -1,20 +1,21 @@
 package com.holysweet.questshop.item;
 
 import com.holysweet.questshop.service.CoinsService;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class CoinItem extends Item {
 
@@ -23,11 +24,18 @@ public class CoinItem extends Item {
     }
 
     @Override
-    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, @NotNull Player player, @NotNull InteractionHand usedHand) {
+    public boolean isFoil(@NotNull ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, @NotNull Player player,
+                                                           @NotNull InteractionHand usedHand) {
         ItemStack itemInHand = player.getItemInHand(usedHand);
 
         if(level.isClientSide()) {
-            level.playSound(player, player.blockPosition(), SoundEvents.EXPERIENCE_ORB_PICKUP, SoundSource.PLAYERS, .5f, 1);
+            level.playSound(player, player.blockPosition(), SoundEvents.EXPERIENCE_ORB_PICKUP, SoundSource.PLAYERS,
+                    .5f, 1);
             return InteractionResultHolder.consume(itemInHand);
         }
 
@@ -35,11 +43,18 @@ public class CoinItem extends Item {
         itemInHand.shrink(amountConsumed);
 
         CoinsService.add((ServerLevel) level, (ServerPlayer) player, amountConsumed);
-        
+
         if(!itemInHand.isEmpty())
             return InteractionResultHolder.success(itemInHand);
 
         player.setItemInHand(usedHand, ItemStack.EMPTY);
         return InteractionResultHolder.consume(itemInHand);
+    }
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack stack, @NotNull TooltipContext context,
+                                @NotNull List<Component> tooltipComponents, @NotNull TooltipFlag tooltipFlag) {
+        tooltipComponents.add(Component.translatable("tooltip.questshop.coin"));
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 }
